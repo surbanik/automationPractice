94e7e927cd9d62dae32c9f46d32156b2058055b1
surbanik <surbanik@sii.pl>
2021-12-22 18:36:05 +0100

    pom update

--- src/test/java/yaml/Loader.java
+++ src/test/java/yaml/Loader.java
@@ -1,11 +1,14 @@
 package yaml;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Loader {
-
+    static private Logger logger = LoggerFactory.getLogger(Loader.class);
     public List<EnvDetails> createEnvList() throws IOException {
         YamlReader yamlReader = new YamlReader();
         EnvDetails test = yamlReader.getConfig().environments.getTest();
@@ -18,16 +21,37 @@ public class Loader {
 
 
     public Loader() throws IOException {
-        for (EnvDetails env : createEnvList()){
-            if (env.getActive().equals("y")){
-                System.setProperty("webURL", env.getWebUrl());
-                System.setProperty("title",env.getTitle());
-                System.setProperty("browser",env.getBrowser());
-                System.setProperty("userEmail",env.getUserEmail());
-                System.setProperty("userPassword",env.getUserPassword());
-                System.setProperty("loginFailMessage",env.getLoginFailMessage());
-                break;
+        if (System.getProperty("Env_Val").isEmpty()) {
+            logger.error("Env_Val is not empty");
+            for (EnvDetails env : createEnvList()) {
+                if (env.getActive().equals("y")) {
+                    System.setProperty("webURL", env.getWebUrl());
+                    System.setProperty("title", env.getTitle());
+                    System.setProperty("browser", env.getBrowser());
+                    System.setProperty("userEmail", env.getUserEmail());
+                    System.setProperty("userPassword", env.getUserPassword());
+                    System.setProperty("loginFailMessage", env.getLoginFailMessage());
+                    break;
+                }
             }
+        }else if(!System.getProperty("Env_Val").isEmpty()){
+            logger.error("Env_Val is empty");
+            for (EnvDetails env : createEnvList()) {
+                if (env.getEnvName().equals(System.getProperty("Env_Val"))) {
+                    System.setProperty("webURL", env.getWebUrl());
+                    System.setProperty("title", env.getTitle());
+                    System.setProperty("browser", env.getBrowser());
+                    System.setProperty("userEmail", env.getUserEmail());
+                    System.setProperty("userPassword", env.getUserPassword());
+                    System.setProperty("loginFailMessage", env.getLoginFailMessage());
+                    break;
+                }
+            }
+
+        }else{
+            logger.error("niepoprawna nazwa środowiska");
         }
     }
+
+
 }
